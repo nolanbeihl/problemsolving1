f866876c89140fe844d371a21de2e17beb9a6681
unknown <nolan.beihl@gmail.com>
2021-10-08 10:42:53 -0600

    progress as of 1130 friday

--- problemsolving1.py
+++ problemsolving1.py
@@ -1,1 +1,19 @@
 #starting on problem sovling 1
+
+word = "Hello"
+reversed_word = ''
+word2 = "hello world"
+capital_word = ''
+
+for index in range(len(word) -1, -1, -1):
+    reversed_word += word[index]
+
+for index2 in range(0, len(word2) , 1):
+
+    capital_word += word2[index2]
+
+print(word)
+print(reversed_word)
+print(word2.upper())
+print(capital_word)
+
