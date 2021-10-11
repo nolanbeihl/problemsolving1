e7afaea66ea9e680812ad9949913b53550e5d537
unknown <nolan.beihl@gmail.com>
2021-10-11 10:09:37 -0600

    I just remembered to do a commit as I was about to finish the worksheet, I will have to work on remembering that

--- problemsolving1.py
+++ problemsolving1.py
@@ -3,17 +3,36 @@
 word = "Hello"
 reversed_word = ''
 word2 = "hello world"
-capital_word = ''
+how_many = "aaaabbbbcccdddd"
 
 for index in range(len(word) -1, -1, -1):
     reversed_word += word[index]
 
-for index2 in range(0, len(word2) , 1):
 
-    capital_word += word2[index2]
-
-print(word)
 print(reversed_word)
-print(word2.upper())
-print(capital_word)
+
+first_letter_capital = word2.title()
+
+print(first_letter_capital)
+
+def compress_string(string):
+    string_value = ""
+    count = 1
+    string_value += string[0]
+    for letter in range(len(string)-1):
+        if(string[letter] == string[letter+1]):
+            count+=1
+        else:
+            if(count > 1):
+                string_value += str(count)
+            string_value += string[letter+1]
+            count = 1
+    if(count > 1):
+        string_value += str(count)
+    return string_value
+
+print(compress_string(how_many))
+print(compress_string("happy"))
+
+
 
